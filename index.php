4bb0058c3998bee56ab99587d1c19e6231fd87e9
alan_omam <omamalan@hotmail.it>
2024-04-02 16:29:39 +0200

    creato la prima istanza della classe

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
- 
+ //inizio classe
  class Movie{
     public $title;
-    public $date;
+    public $year;
     public $mainCharacter;
     public $discription;
   
-  function __construct($_title, $_date , $_mainCharacter) {
+  function __construct($_title, $_year , $_mainCharacter) {
 
     $this->title = $_title;
-    $this->date = $_date;
+    $this->year = $_year;
     $this->mainCharacter = $_mainCharacter;
   }
 
-
-
  }
+ //fine classe
 
 
+ $movie1 = new Movie("TITANIC","2000","leonardo di caprio");
+ var_dump($movie1)
 
 ?>
 
@@ -29,6 +30,9 @@
     <title>OOP</title>
 </head>
 <body>
+
+
+
 
  <h1>titolo</h1>
     
